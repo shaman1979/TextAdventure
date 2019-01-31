bfd787c640072b361a4d8fe057b21fc238fb1e66
shaman1979 <upgamestudio@gmail.com>
2019-01-31 21:34:03 +0300

    add main game mechanic

---
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text storyText;
+
+    [SerializeField] State startState;
+
+    State state;
+
+    private void Start()
+    {
+        state = startState;
+        storyText.text = state.GetStory();
+    }
+
+    private void Update()
+    {
+        StoryNext();
+    }
+
+    private void StoryNext()
+    {
+        var nextStory = state.GetNextState();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStory[0];
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStory[1];
+        }
+
+        storyText.text = state.GetStory();
+    }
+}
---
+++ Text101/Assets/Scripts/State.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(14,10)][SerializeField] string story;
+    [SerializeField] State[] nextState;
+
+    public State[] GetNextState()
+    {
+        return nextState;
+    }
+
+    public string GetStory()
+    {
+        return story;
+    }
+	
+}
